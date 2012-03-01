b803ebefefa739d78d567839fbbcd2402ed3ef5c
Shawn Veader <shawn@veader.org>
2012-03-01 11:42:21 -0500

    Remove while loop with repeated rand calls in favor of shuffling original array of hashes.

--- lib/randumb.rb
+++ lib/randumb.rb
@@ -29,17 +29,12 @@ module Randumb
         id_only_relation = relation.select("#{table_name}.id")
         id_results = connection.select_all(id_only_relation.to_sql)
       
-        ids = {}
-      
-        while( ids.length < max_items && ids.length < id_results.length )
-          rand_index = rand( id_results.length )
-          ids[rand_index] = id_results[rand_index]["id"] unless ids.has_key?(rand_index)
-        end
+        ids = id_results.shuffle[0,max_items].collect { |h| h['id'] }
   
         the_scope = klass.includes(original_includes)
         # specifying empty selects caused bug in rails 3.0.0/3.0.1
         the_scope = the_scope.select(original_selects) unless original_selects.empty? 
-        records = the_scope.find_all_by_id(ids.values)
+        records = the_scope.find_all_by_id(ids)
                 
         if return_first_record
           records.first
