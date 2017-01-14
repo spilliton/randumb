093310ed659e3215176c7555e3965595da28102b
Zachary Kloepping <zachary@massrelevance.com>
2017-01-14 14:15:17 -0600

    arg

--- test/models/artist.rb
+++ test/models/artist.rb
@@ -1,10 +1,10 @@
 class Artist < ActiveRecord::Base
   has_many :albums
 
-  if Gem::Version.new(ActiveRecord::VERSION::STRING) >= Gem::Version.new('3.2')
-    default_scope { where("deleted_at IS NULL") }
+  if Gem::Version.new(ActiveRecord::VERSION::STRING) >= Gem::Version.new("3.2")
+    default_scope { where("artists.deleted_at IS NULL") }
   else
-    default_scope where("deleted_at IS NULL")
+    default_scope where("artists.deleted_at IS NULL")
   end
 
   scope :at_least_three_views, -> { where("views >= 3") }
